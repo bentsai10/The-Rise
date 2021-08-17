0171d8f53f50941ce2f6a6e09412bfbe03a4bb22
bentsai10 <benjamintsa@gmail.com>
2021-08-17 16:22:09 -0700

    temporarily use default db

--- humanely_digital/settings.py
+++ humanely_digital/settings.py
@@ -94,15 +94,21 @@ if 'RDS_DB_NAME' in os.environ:
     }
 else:
     DATABASES = {
-        'default' : {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME' : 'HUMANELY_DIGITAL_DB',
-            'USER' : 'postgres', 
-            'PASSWORD': os.environ['POSTGRES_PW'],
-            'HOST' : 'localhost',
-            'PORT' : '5432',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+    # DATABASES = {
+    #     'default' : {
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #         'NAME' : 'HUMANELY_DIGITAL_DB',
+    #         'USER' : 'postgres', 
+    #         'PASSWORD': os.environ['POSTGRES_PW'],
+    #         'HOST' : 'localhost',
+    #         'PORT' : '5432',
+    #     }
+    # }
 
 
 # Password validation
