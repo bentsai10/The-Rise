062888f61a3c074639e3f38d77277c9d62f6e2c5
bentsai10 <benjamintsa@gmail.com>
2021-10-18 03:33:50 -0400

    testing media_url with logger

--- main/views.py
+++ main/views.py
@@ -459,12 +459,13 @@ def process_discussion_post (request):
                     new_filename = filename[:-4]
                     stream = ffmpeg.input(settings.MEDIA_ROOT + '/audio/discussions/'+ str(user.id) + '/' + filename)
                     logger = logging.getLogger("django")
-                    logger.debug('\n\n\n\n' + settings.MEDIA_ROOT + '/audio/discussions/'+ str(user.id) + '/' + new_filename  + "0.mp3" +'\n\n\n\n')
+                    logger.debug('\n\n\n\n' + settings.MEDIA_URL + '/audio/discussions/'+ str(user.id) + '/' + new_filename  + "0.mp3" +'\n\n\n\n')
                     stream = ffmpeg.output(stream, settings.MEDIA_ROOT + '/audio/discussions/'+ str(user.id) + '/' + new_filename  + "0.mp3")
                     ffmpeg.run(stream, capture_stdout=True, capture_stderr=True)
+                    new_disc = Discussion.objects.create(title = title, participant_cap =  participant_cap, audio = '/audio/discussions/'+ str(user.id) + '/' + new_filename  + "0.mp3", poster = user, space = space, duration = duration)
                 except Exception as e:
                     logger.debug(e.stderr)
-                new_disc = Discussion.objects.create(title = title, participant_cap =  participant_cap, audio = '/audio/discussions/'+ str(user.id) + '/' + new_filename  + "0.mp3", poster = user, space = space, duration = duration)
+                
                 if len(link) > 0:
                     try:
                         link_html = urlopen(link).read()
